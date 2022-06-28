dc938f3b2990491e70234661eace9460c02235d1
minidiscs <shanedbutler@gmail.com>
2022-06-28 11:07:01 -0400

    function added and exported

--- src/scripts/PotteryCatalog.js
+++ src/scripts/PotteryCatalog.js
@@ -1,1 +1,16 @@
 let potteryCatalog = []
+
+export const toSellOrNotToSell = (pot) => {
+    if (pot.cracked === false) {
+        if (pot.weight >= 6) {
+            pot.price = 40
+        }
+        else {
+            pot.price = 20
+        }
+            potteryCatalog.push(pot)
+    }
+    return pot
+}
+
+export const usePottery = () => potteryCatalog.map(pots => ({...pots}))
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
 // Imports go first
-import { makePottery } from "./PotteryWheel.js";
-import { firePottery } from "./Kiln.js";
+import { makePottery } from "./PotteryWheel.js"
+import { firePottery } from "./Kiln.js"
+import { toSellOrNotToSell, usePottery} from "./PotteryCatalog.js"
 
 // Make 5 pieces of pottery at the wheel
 
@@ -10,6 +11,7 @@ let vase = makePottery("vase", 70, 50)
 let plate = makePottery("plate", 45, 5)
 let jug = makePottery("jug", 60, 35)
 
+console.log(mug)
 
 // Fire each piece of pottery in the kiln
 let finishedMug = firePottery(mug, 2100)
@@ -18,12 +20,19 @@ let finishedVase = firePottery(vase, 2100)
 let finishedPlate = firePottery(plate, 2100)
 let finishedJug = firePottery(jug, 2300)
 
-
-
 console.log(finishedMug)
 
 // Determine which ones should be sold, and their price
+let pricedMug = toSellOrNotToSell(finishedMug)
+let pricedBowl = toSellOrNotToSell(finishedBowl)
+let pricedVase = toSellOrNotToSell(finishedVase)
+let pricedPlate = toSellOrNotToSell(finishedPlate)
+let pricedJug = toSellOrNotToSell(finishedJug)
 
+console.log(pricedMug)
+
+let potteryCatalog = usePottery()
+console.log(potteryCatalog)
 
 // Invoke the component function that renders the HTML list
 
