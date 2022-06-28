b746646279a682095644f793761c4d6f7e3ea99c
minidiscs <shanedbutler@gmail.com>
2022-06-28 10:26:24 -0400

    kiln function completed

---
+++ src/scripts/Kiln.js
@@ -0,0 +1,10 @@
+export const firePottery = (potteryObject, temperature) => {
+    potteryObject.fired = true
+    if (temperature > 2200) {
+        potteryObject.cracked = true
+    }
+    else {
+        potteryObject.cracked = false
+    }
+    return potteryObject
+}
